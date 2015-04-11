95fed9b5815adeb5ffbd416e64c4124e662b13ae
Alf Eaton <hubgit@@users.noreply.github.com>
2015-04-11 20:41:33 +0100

    Only serve from the cache when offline

--- serviceworker.js
+++ serviceworker.js
@@ -3,18 +3,16 @@ importScripts('serviceworker-cache-polyfill.js');
 // TODO: cache figures and other external resources on install
 
 self.onfetch = function(event) {
-    event.respondWith(caches.match(event.request).then(function (response) {
-        if (response) {
-            return response;
-        }
+    var fetchRequest = event.request.clone();
 
-        var fetchRequest = event.request.clone();
+    event.respondWith(fetch(fetchRequest).then(function (response) {
+        caches.open('responses').then(function (cache) {
+            cache.put(fetchRequest, response.clone());
+        });
 
-        return fetch(fetchRequest).then(function (response) {
-            caches.open('responses').then(function (cache) {
-                cache.put(fetchRequest, response.clone());
-            });
-
+        return response;
+    }, function () {
+        return caches.match(event.request).then(function (response) {
             return response;
         });
     }));
